bcaa0c4918e69a1eaacd693d106412c95900e04a
jesuisbienbien <60805847+jesuisbienbien@users.noreply.github.com>
2022-04-13 23:44:14 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const shareButton = document.getElementById("share-button");
+const shareButtonDark = document.getElementById("share-button-dark");
+const authorInfo = document.getElementById("author-container");
+const socialMedia = document.getElementById("social-media");
+const iconShare = document.getElementById("icon-share-img");
+const width = window.innerWidth;
+
+
+shareButton.addEventListener("click", (e) => {
+    console.log(width);
+    if(width < 1440) {
+        authorInfo.classList.add("hidden");
+        socialMedia.classList.remove("hidden");
+    } else {
+        iconShare.classList.add("filter-white");
+        shareButton.classList.remove("bright-theme");
+        shareButton.classList.add("dark-theme");
+        shareButtonDark.classList.add("hidden");
+        socialMedia.classList.remove("hidden");
+    }
+    
+})
