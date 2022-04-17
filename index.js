6efc2800e22311c15e52fd70b61c94bba4b0629a
jesuisbienbien <60805847+jesuisbienbien@users.noreply.github.com>
2022-04-16 22:20:12 -0700

    added js - hide share section

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
 const shareButton = document.getElementById("share-button");
 const shareButtonDark = document.getElementById("share-button-dark");
 const authorInfo = document.getElementById("author-container");
-const socialMedia = document.getElementById("social-media");
+const shareSection = document.getElementById("share-section");
 const iconShare = document.getElementById("icon-share-img");
 const triangle = document.getElementById("triangle-down");
 const width = window.innerWidth;
 
+function hide(element) {
+    element.classList.add("hidden");
+}
+function show(element) {
+    element.classList.remove("hidden");
+}
 
-shareButton.addEventListener("click", (e) => {
-    console.log(width);
+function reset(width){
+    //if width < 1440px, show author
+    //else if width >= 1440px return share button to bright theme 
     if(width < 1440) {
-        authorInfo.classList.add("hidden");
-        socialMedia.classList.remove("hidden");
-    } else {
+        show(authorInfo);
+    } else{
+        shareButton.classList.remove("dark-theme");
+        shareButton.classList.add("bright-theme");
+        iconShare.classList.remove("filter-white");
+        hide(triangle);
+    }
+    //hide share section
+    hide(shareSection);
+
+    
+}
+
+function showShareSection(width) {
+    //hide author if width <1440px
+    //hide shareButton (Dark) if width >= 1440px and change the shareButton to dark theme
+
+
+    if(width < 1440){
+        hide(authorInfo);
+    }else {
         iconShare.classList.add("filter-white");
         shareButton.classList.remove("bright-theme");
         shareButton.classList.add("dark-theme");
-        shareButtonDark.classList.add("hidden");
-        socialMedia.classList.remove("hidden");
-        triangle.classList.remove("hidden");
+        hide(shareButtonDark);
+        show(triangle);
     }
+
+
+    //remove hidden class on share section  
+    show(shareSection);
+}
+
+/* if shareButton (original) is clicked */
+shareButton.addEventListener("click", (e) => {
+   //check if the share section hidden or not
+   if(shareSection.classList.contains("hidden")) {
+       showShareSection(width);
+   }else {
+       reset(width);
+   }
     
+})
+
+/* if shareButtonDark is clicked (on mobile) */
+shareButtonDark.addEventListener("click", (e) => {
+    reset(width);
 })
